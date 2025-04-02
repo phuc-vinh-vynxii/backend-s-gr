aaffd4028d315af1db8867987b3158ddb215b36a
Phuc Vinh <nguyenvinh19525@gmail.com>
2025-04-02 21:43:43 +0700

    crud

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
-
-app.get('', (req, res) => {
-    res.send("Hello World!");
-})
-
-
+app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
